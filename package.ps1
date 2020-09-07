85a3b4c1e7a5db8e9483ca4e36af2a494f031e7b
LP <grofit1@yahoo.com>
2020-09-07 21:51:10 +0100

    Attempting to fix gitpage issue

--- package.ps1
+++ package.ps1
@@ -1,2 +1,5 @@
 dotnet publish -c Release src\OpenRpg.Demos.Web.sln
-Copy-Item -Path "src\OpenRpg.Demos.Web\bin\Release\netstandard2.1\publish\wwwroot\*" -Destination "dist" -Recurse
+Remove-Item -Recurse -Force 'docs'
+md -path 'docs'
+Copy-Item -Path "src\OpenRpg.Demos.Web\bin\Release\netstandard2.1\publish\wwwroot\*" -Destination "docs" -Recurse
+((Get-Content -path "docs\index.html" -Raw) -replace '<base href="/" />', '<base href="/OpenRpg.Demos.Web/" />') | Set-Content -Path "docs\index.html"
