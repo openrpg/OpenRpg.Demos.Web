6b7c8fbe794a9056cd10fce9690b8fc73cb17526
LP <grofit1@yahoo.com>
2022-03-25 13:34:02 +0000

    Moving to use latest blazor and .net 6

--- src/OpenRpg.Demos.Web/Program.cs
+++ src/OpenRpg.Demos.Web/Program.cs
@@ -1,25 +1,12 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using OpenRpg.Demos.Web;
 using OpenRpg.Demos.Web.Extensions;
 using OpenRpg.Demos.Web.Modules;
 
-namespace OpenRpg.Demos.Web
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddModule<OpenRpgModule>();
+builder.Services.AddModule<OpenRpgDataModule>();
+await builder.Build().RunAsync();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddModule<OpenRpgModule>();
-            builder.Services.AddModule<OpenRpgDataModule>();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
