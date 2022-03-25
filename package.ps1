f606483db93cf0ff520c3598cfbdfaf3d8209ee9
LP <grofit1@yahoo.com>
2022-03-25 16:10:50 +0000

    Updated docs to new version

--- package.ps1
+++ package.ps1
@@ -1,5 +1,5 @@
 dotnet publish -c Release src\OpenRpg.Demos.Web.sln
 Remove-Item -Recurse -Force 'docs'
 md -path 'docs'
-Copy-Item -Path "src\OpenRpg.Demos.Web\bin\Release\netstandard2.1\publish\wwwroot\*" -Destination "docs" -Recurse
+Copy-Item -Path "src\OpenRpg.Demos.Web\bin\Release\net6.0\publish\wwwroot\*" -Destination "docs" -Recurse
 ((Get-Content -path "docs\index.html" -Raw) -replace '<base href="/" />', '<base href="/OpenRpg.Demos.Web/" />') | Set-Content -Path "docs\index.html"
